59f0f42e9dad04227371c3b2701c24bd672744b6
Jonas Westenhoff <jwestenhoff@web.de>
2021-10-23 16:21:52 +0200

    replace static by dynamic list

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,24 +19,31 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
   Widget _buildList() {
-    return ListView(padding: const EdgeInsets.all(8), children: <Widget>[
-      Container(
-        height: 50,
-        color: Colors.amber[600],
-        child: const Center(child: Text("First Entry")),
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final index = item ~/ 2;
+
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 15),
       ),
-      Container(
-        height: 50,
-        color: Colors.amber[700],
-        child: const Center(child: Text("Second Entry")),
-      ),
-      Container(
-        height: 50,
-        color: Colors.amber[800],
-        child: const Center(child: Text("Third Entry")),
-      ),
-    ]);
+    );
   }
 
   Widget build(BuildContext context) {
