cf2141a72ffc15141f690abc155b00f4058cafe3
Jonas Westenhoff <jwestenhoff@web.de>
2021-10-23 16:24:54 +0200

    move random words stuff in own file

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,43 +13,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
 
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return Divider();
-
-          final index = item ~/ 2;
-
-          if (index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_randomWordPairs[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(fontSize: 15),
-      ),
-    );
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Center(child: Text('WordPair Generator'))),
-      body: _buildList(),
-    );
-  }
-}
