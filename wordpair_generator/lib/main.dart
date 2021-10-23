bfa9e5b01c55d5bf40cb0b572812e722e159c1d7
Jonas Westenhoff <jwestenhoff@web.de>
2021-10-23 16:05:55 +0200

    create random words area for body

--- wordpair_generator/lib/main.dart
+++ wordpair_generator/lib/main.dart
@@ -7,11 +7,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.lightGreenAccent[400]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('WordPair Generator')),
-        ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    return ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+      Container(
+        height: 50,
+        color: Colors.amber[600],
+        child: const Center(child: Text("First Entry")),
       ),
+      Container(
+        height: 50,
+        color: Colors.amber[700],
+        child: const Center(child: Text("Second Entry")),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[800],
+        child: const Center(child: Text("Third Entry")),
+      ),
+    ]);
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('WordPair Generator'))),
+      body: _buildList(),
     );
   }
 }
